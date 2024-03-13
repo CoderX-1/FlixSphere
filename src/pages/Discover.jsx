4f2fc2b32d82bd2186028d738c2202a04f2c1061
Muhammed Ayaan <kuchicheena@gmail.com>
2024-03-13 22:38:50 +0500

    commit

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./Navbar";
+import Navbar from "../components/Navbar";
 import { TMDB_API_KEY, endpoints, TMDB_URL } from "../services/Tmdb";
 import { useState, useEffect } from "react";
 import axios from "axios";
