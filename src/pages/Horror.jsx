411f408002663442bdff3be4307b54503a6ec0d1
Muhammed Ayaan <kuchicheena@gmail.com>
2024-03-16 14:22:13 +0500

    commit

--- src/pages/Horror.jsx
+++ src/pages/Horror.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import VerticalCard from "@/components/verticalCard";
+import VerticalCard from "@/components/VerticalCard";
 import { endpoints, TMDB_URL } from "../services/Tmdb";
 
 const Page = ({ items, title, link }) => {
