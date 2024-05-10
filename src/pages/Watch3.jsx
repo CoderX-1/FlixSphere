91c05623e32bb501f16ccf9e12e562d6d5b481db
Muhammed Ayaan <littlepanda024@gmail.com>
2024-05-10 10:55:12 +0500

    fixed

--- src/pages/Watch3.jsx
+++ src/pages/Watch3.jsx
@@ -45,8 +45,8 @@ const Watch2 = () => {
       allowFullScreen
       src={
         type === "movie"
-          ? `https://estore.digital/embed1/player.php?id=${id}`
-          : `https://estore.digital/embed1/player.php?id=${id}/${season}/${episode}/`
+          ? `https://player.autoembed.cc/embed/movie/${id}`
+          : `https://player.autoembed.cc/embed/tv/${id}/${season}/${episode}/`
       }
       width="100%"
       height="100%"
