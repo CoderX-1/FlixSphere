10ea78d3975b07a31e403ae426ff89f96bcae71e
Muhammed Ayaan <littlepanda024@gmail.com>
2024-06-19 12:16:15 +0500

    Remove "loading" prop from Trendingrow component.

--- src/components/Trendingrow.jsx
+++ src/components/Trendingrow.jsx
@@ -108,7 +108,6 @@ const Trendingrow = () => {
                             background:
                               "linear-gradient(to bottom, rgba(0, 0, 0, 0.3) 0%, rgba(0, 0, 0, 0) 100%)",
                           }}
-                          loading="lazy"
                           removeWrapper
                           radius="none"
                         />
